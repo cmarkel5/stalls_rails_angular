07bc436533ff9de9b4838f0829cc2de66c125dfe
cmarkel5 <cmarkel5@gmail.com>
2014-11-12 14:01:33 -0500

    first commit

---
+++ app/assets/javascripts/static.js
@@ -0,0 +1,37 @@
+angular.module('leatherLaneMarketApp', ['ngAnimate'])
+ .controller('MarketController', ["$scope", function($scope){
+   $scope.stalls = stalls;
+
+   $scope.selectStall = function(stall) {
+    $scope.selectedStall = stall;
+   }
+
+   $scope.clearSelectedStall = function() {
+    $scope.selectedStall = false;
+   }
+
+   $scope.addStall = function() {
+    $scope.stalls.push($scope.newStall);
+    $scope.newStall = false;
+    $scope.stallForm.$setPristine();
+   }
+
+ }]);
+
+stalls = [
+  {
+    name: "Burrito",
+    price: 5,
+    description:  "Meat and vegetables in a delicious wrap"
+  },
+  {
+    name: "Pizza",
+    price: 6.5,
+    description:  "Cheese and meat and veg on some dough"
+  },
+  {
+    name: "Fallafel",
+    price: 4.5,
+    description: "Deep fried delicious chickpeas"
+  }
+]
